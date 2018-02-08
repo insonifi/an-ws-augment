329045bae9b0124262f9a45276406974179ff9a1
Andrey P <andrey.panteleev@ru.axxonsoft.com>
2018-02-08 14:49:43 +0300

    WS Augment Proxy

---
+++ handlers.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"context"
+	"encoding/binary"
+	"io/ioutil"
+	"log"
+	"mime"
+	"mime/multipart"
+	"net/http"
+	"time"
+)
+
+const NOTIME = 0
+
+func GetData(
+	req *http.Request,
+	cmd WSCmd,
+	wschan chan []byte,
+	pending chan Signal,
+) {
+	client := &http.Client{}
+	ctx, cancel := context.WithCancel(context.Background())
+	reqctx := req.WithContext(ctx)
+	log.Println("requesting", req.URL.RequestURI())
+	res, err := client.Do(reqctx)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	mediaType, params, err := mime.ParseMediaType(res.Header.Get("Content-Type"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	prefix := append([]byte{0, byte(len(cmd.Endpoint))}, []byte(cmd.Endpoint)...)
+	pkt_hdr := append(prefix/*, []byte(FCC[cmd.Format])...*/)
+
+	var read StreamReader
+
+	switch mediaType {
+	case "multipart/x-mixed-replace":
+		read = readMultiPart(&pkt_hdr, params["boundary"], res)
+	case "image/jpeg":
+		ts := getTime(req.Header.Get("X-Video-Original-Time"))
+		pkt, err := readHttpWhole(&pkt_hdr, res, ts)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		wschan <- pkt
+		<-pending
+		return
+	case "video/mp4":
+		read = readHttpStream(&pkt_hdr, res)
+	}
+	log.Println("pulling")
+L:
+	for {
+		select {
+		case <-pending:
+			cancel()
+			log.Println("STOPPED", cmd.Endpoint)
+			break L
+		default:
+			pkt, err := read()
+			if err != nil {
+				log.Println(err)
+				break L
+			}
+			wschan <- pkt
+		}
+	}
+}
+
+/**
+ * Stream Handlers
+ */
+
+func readHttpStream(hdr *[]byte, res *http.Response) StreamReader {
+	buf := make([]byte, BUF_SIZE)
+
+	log.Print("long-polling")
+	return func() ([]byte, error) {
+		n, err := res.Body.Read(buf)
+		if err != nil {
+			return nil, err
+		}
+		log.Println("read", n)
+		pkt := makePacket(hdr, NOTIME, buf[:n])
+
+		return pkt, nil
+	}
+}
+
+func readMultiPart(
+	hdr *[]byte,
+	boundary string,
+	res *http.Response,
+) StreamReader {
+	mp := multipart.NewReader(res.Body, boundary)
+
+	log.Print("multipart")
+	return func() ([]byte, error) {
+		part, err := mp.NextPart()
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+
+		frame, err := ioutil.ReadAll(part)
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+
+    ts := getTime(part.Header.Get("X-Video-Original-Time"))
+
+		pkt := makePacket(hdr, ts, frame)
+
+		return pkt, nil
+	}
+}
+
+func readHttpWhole(
+	hdr *[]byte,
+	res *http.Response,
+	ts uint64,
+) ([]byte, error) {
+	log.Println("read whole")
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	pkt := makePacket(hdr, ts, body)
+
+	return pkt, nil
+}
+
+func makePacket(
+	pkt_hdr *[]byte,
+	t uint64,
+	payload []byte,
+) []byte {
+	tsBytes := make([]byte, 8)
+
+	binary.BigEndian.PutUint64(tsBytes, uint64(t))
+	pkt := append((*pkt_hdr), tsBytes...)
+	pkt = append(pkt, payload...)
+
+	return pkt
+}
+
+func getTime(strtime string) uint64 {
+	if strtime != "" {
+		ts, _ := time.Parse(
+			ASIP_TIMEFORMAT,
+			strtime,
+		)
+		return uint64(ts.UnixNano() / time.Millisecond.Nanoseconds())
+	}
+  return NOTIME
+}
---
+++ main.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/gorilla/websocket"
+)
+
+type dataHandler func(net.Conn)
+type WSCmd struct {
+	Archive   string
+	Method    string
+	Endpoint  string
+	BeginTime string
+	Format    string
+	StreamId	string
+	Speed     int
+	Width     int
+	Height    int
+	Q         int
+}
+type ReadableHeader interface {
+	Get(name string) string
+}
+type Signal struct{}
+type StreamReader func() ([]byte, error)
+
+const (
+	ARCHIVE         = "archive"
+	ASIP_TIMEFORMAT = "20060102T150405.999999"
+	BUF_SIZE        = 1 << 20 // 1K; << 16 // 64K
+	LIVE            = "live"
+	M_PLAY          = "play"
+	M_STOP          = "stop"
+)
+
+var (
+	backend  string
+	port     int
+	upgrader = websocket.Upgrader{}
+	FCC      = map[string]string{
+		"mjpeg": "mjpg",
+		"mp4":   "fmp4",
+	}
+)
+
+func video(w http.ResponseWriter, r *http.Request) {
+	log.Println("WebSocket connection")
+	wsc, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade: ", err)
+		return
+	}
+
+	wschan := make(chan []byte)
+	pending := make(map[string]chan Signal)
+
+	go handleData(wsc, wschan, &pending)
+
+	wsc.SetCloseHandler(func(code int, text string) error {
+		cleanup(&pending)
+		wsc.Close()
+		log.Println("Closing WebSocket")
+
+		return nil
+	})
+
+	for {
+		var cmd WSCmd
+
+		err := wsc.ReadJSON(&cmd)
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		handleWSCmd(wschan, &pending, cmd)
+	}
+}
+
+func handleData(
+	wsc *websocket.Conn,
+	wschan chan []byte,
+	pending *map[string]chan Signal,
+) {
+	for {
+		select {
+		case pkt := <-wschan:
+			//log.Println("wsc:", "send", len(pkt))
+			err := wsc.WriteMessage(websocket.BinaryMessage, pkt)
+			if err != nil {
+				log.Println("wsc:", err)
+				cleanup(pending)
+
+				return
+			}
+		}
+	}
+}
+
+func cleanup(pending *map[string]chan Signal) {
+	for endpoint, _ := range *pending {
+		stopStream(pending, endpoint)
+	}
+}
+
+func stopStream(pending *map[string]chan Signal, endpoint string) {
+	if quit, ok := (*pending)[endpoint]; ok {
+		log.Println("cleanup", endpoint)
+		delete(*pending, endpoint)
+		quit <- Signal{}
+		close(quit)
+		log.Println("clenup done")
+	}
+}
+
+func makeRequest(url string, opts map[string]interface{}) (req *http.Request) {
+	req, err := http.NewRequest("GET", url, bytes.NewReader(nil))
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	req.SetBasicAuth("root", "root")
+	query := req.URL.Query()
+
+	for key, value := range opts {
+		query.Add(key, fmt.Sprint(value))
+	}
+	req.URL.RawQuery = query.Encode()
+
+	return req
+}
+
+func handleWSCmd(
+	wschan chan []byte,
+	pending *map[string]chan Signal,
+	cmd WSCmd,
+) {
+	log.Println(cmd)
+	switch cmd.Method {
+	case M_PLAY:
+		opts := make(map[string]interface{})
+
+		source := LIVE
+		begintime := ""
+		if cmd.BeginTime != "" {
+			source = ARCHIVE
+			begintime = fmt.Sprintf("/%s", cmd.BeginTime)
+			opts["arhive"] = cmd.Archive
+		}
+		url := fmt.Sprintf(
+			"%s/%s/media/%s%s",
+			backend, source, cmd.Endpoint, begintime,
+		)
+
+		opts["format"] = cmd.Format
+
+		if cmd.Speed > 0 {
+			opts["speed"] = cmd.Speed
+		}
+
+		if cmd.Format == "jpeg" {
+			opts["w"] = cmd.Width
+			opts["h"] = cmd.Height
+			opts["vc"] = cmd.Q
+		}
+
+		//stop pending streams
+		stopStream(pending, cmd.Endpoint)
+		quit := make(chan Signal)
+
+		(*pending)[cmd.Endpoint] = quit
+
+		req := makeRequest(url, opts)
+		req.Header.Set("X-Video-Original-Time", cmd.BeginTime)
+
+		go GetData(req, cmd, wschan, quit)
+	case M_STOP:
+		stopStream(pending, cmd.Endpoint)
+
+		return
+	}
+}
+
+func main() {
+	flag.StringVar(&backend, "backend", "http://localhost:8000", "Backend URI")
+	flag.IntVar(&port, "listen", 9999, "Listening port")
+	flag.Parse()
+	mux := http.NewServeMux()
+	bURL, err := url.Parse(backend)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mux.HandleFunc("/ws", video)
+	mux.Handle("/", httputil.NewSingleHostReverseProxy(bURL))
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), mux))
+}
